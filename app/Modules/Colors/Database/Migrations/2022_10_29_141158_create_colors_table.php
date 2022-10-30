fbf1df49ab1b35e12c769ae5fc90bf9f33370c57
Vypsen <s.semenets2001@mail.ru>
2022-10-30 17:19:43 +1000

    fix color size

--- app/Modules/Colors/Database/Migrations/2022_10_29_141158_create_colors_table.php
+++ app/Modules/Colors/Database/Migrations/2022_10_29_141158_create_colors_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('color_name');
             $table->timestamps();
         });
     }
