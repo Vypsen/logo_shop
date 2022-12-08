8f1ce034b04e54f8e867ae3222c448c3a9d628f4
Vypsen <s.semenets2001@mail.ru>
2022-12-08 23:09:03 +1000

    clear module order

---
+++ app/Modules/Order/Database/Migrations/2022_12_08_110620_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('city');
+            $table->string('index')->nullable();
+            $table->string('street');
+            $table->string('house');
+            $table->string('apartment')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
